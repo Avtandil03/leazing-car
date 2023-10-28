3949cfcfce8846da2a08d70ade7a78ab67624417
Avtandil Saparov <avtanndil@gmail.com>
2023-10-28 19:37:40 +0300

    add onSubmit

--- src/components/ModalApplication/ModalApplication.tsx
+++ src/components/ModalApplication/ModalApplication.tsx
@@ -1,7 +1,8 @@
 import { ModalWrapper } from "@/shared/ui";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import s from "./modalApplication.module.scss";
 import { Button, Input, Svg } from "..";
+import { isPhoneValid } from "@/shared/helpers";
 
 interface ModalApplicationProps {
   isOpen: boolean;
@@ -12,10 +13,20 @@ export const ModalApplication: FC<ModalApplicationProps> = ({
   isOpen = false,
   onClose
 }) => {
+
+  function onSubmit(e:any){
+    e.preventDefault()
+    if(!isPhoneValid(phone) ||  !name.length) return 
+    alert(JSON.stringify({name: name, phone: phone}))
+  }
+  
+
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
   return (
     <ModalWrapper isOpen={isOpen} onClose={onClose} className={s.modalWrapper}>
-      <form className={s.modal}>
-        <button className={s.closeBtn} onClick={() => onClose()}>
+      <form className={s.modal} onSubmit={onSubmit}>
+        <button type='button' className={s.closeBtn} onClick={() => onClose()}>
           <Svg name="close-modal" />
         </button>
         <div className={s.modalContent}>
@@ -26,19 +37,34 @@ export const ModalApplication: FC<ModalApplicationProps> = ({
           </p>
           <div className={s.inputs}>
             <Input
-              onChange={() => {}}
-              value=""
+              onChange={(val) => setPhone(val)}
+              value={phone}
               label="Телефон *"
               placeholder="+7 (921) 123 45 67"
+              name="phone"
+              needValidation
+              isValid={isPhoneValid(phone)}
+              errText="Incorrect phone"
             />
-            <Input onChange={() => {}} value="" placeholder="Имя" />
+            <Input 
+              onChange={(val) => setName(val)} 
+              value={name}
+              placeholder="Имя" 
+              name="name"
+              needValidation
+              isValid={!!name.length}
+            />
           </div>
           <div className={s.sendingBlock}>
             <p className={s.agreementInfo}>
               Нажимая на кнопку «Оставить заявку», я даю согласие{" "}
               <a href="#personal-data">на обработку персональных данных</a>
             </p>
-            <Button variant="primary" onClick={() => {}}>
+            <Button variant="primary" 
+              onClick={() => {}} 
+              type="submit" 
+              disabled={!isPhoneValid(phone) || !name.length}
+            >
               Оставить заявку
             </Button>
           </div>
